7adefa7ae60df854e4977a7fb0ec83a82e163ed7
eXiled4 <elite21ret@gmail.com>
2022-07-26 10:27:20 +1000

    sort function added

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-declare const genRandomNumbers:any;
+declare const genRandomNumbers: any;
 
 @Component({
   selector: 'app-second',
@@ -9,17 +9,28 @@ declare const genRandomNumbers:any;
 })
 export class SecondComponent implements OnInit {
 
-  rNum = <[]>genRandomNumbers();
-  page = 1;
-  itemsToDisplay = 10;
-  // Property '...' has no initializer and is not definitely assigned in the constructor - fixed with "strictPropertyInitialization": false (in tsconfig.json)
-  // Make sure you add that after "strict": true else the transpiler seems to turn it on again 
 
-  pageChanged(event){
-    this.page = event
-  }
+    ranNum = <[]> genRandomNumbers().sort(function(x, y){
+      return x - y
+    });
+    page = 1;
+    itemsToDisplay = 10;
 
-  // Typescript: TS7006: Parameter 'xxx' implicitly has an 'any' type - fixed with "noImplicitAny": false (in tsconfig.json)
+    pageChanger(event){
+      this.page = event
+    }
+
+  // rNum = <[]>genRandomNumbers();
+  // page = 1;
+  // itemsToDisplay = 10;
+  // // Property '...' has no initializer and is not definitely assigned in the constructor - fixed with "strictPropertyInitialization": false (in tsconfig.json)
+  // // Make sure you add that after "strict": true else the transpiler seems to turn it on again 
+
+  // pageChanged(event){
+  //   this.page = event
+  // }
+
+  // // Typescript: TS7006: Parameter 'xxx' implicitly has an 'any' type - fixed with "noImplicitAny": false (in tsconfig.json)
 
   constructor() { }
 
